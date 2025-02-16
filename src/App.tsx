96a24e1617af7d8ecd2f2759b9b0b6be2390605a
kvarcev <0-zh@mail.ru>
2025-02-16 13:39:21 +0400

    mb end

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("");
+  const noRef = useRef<HTMLButtonElement>(null);
+  const [yesClicked, setYesClicked] = useState(false);
+
+  useEffect(() => {
+    setFirstName(window.Telegram.WebApp.initDataUnsafe.user?.first_name || "Аноним");
+  }, []);
+
+  const handleNoClick = () => {
+    if (noRef.current) {
+      const randX = Math.floor(Math.random() * 60 - noRef.current.offsetWidth);
+      const randY = Math.floor(Math.random() * 320 - noRef.current.offsetHeight);
+
+      noRef.current.style.transform = `translate(${randX}px, ${randY}px)`;
+    }
+  };
+
+  const handleYesClick = () => {
+    setYesClicked(true);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="wrapper">
+      {yesClicked && (
+        <div>
+          <div className="title">{firstName}, я тоже тебя очень сильно люблю! 💖</div>
+        </div>
+      )}
+      {!yesClicked && (
+        <div>
+          <div className="title">{firstName} ты меня любишь?</div>
+          <div className="btns">
+            <button onClick={handleYesClick} className="button yes">
+              Да
+            </button>
+            <button onClick={handleNoClick} ref={noRef} className="button no">
+              Нет
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
